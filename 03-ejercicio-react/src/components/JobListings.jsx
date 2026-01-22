83acf1ab786277486c12709c4fbe526832ee4502
madeval <alvarez.fing@gmail.com>
2026-01-22 18:47:35 -0300

    Add 03-ejercicio-react/src/components/JobListings.jsx

--- 03-ejercicio-react/src/components/JobListings.jsx
+++ 03-ejercicio-react/src/components/JobListings.jsx
@@ -2,14 +2,13 @@ import JobCard from "./JobCard.jsx";
 
 
 const JobListings = ({ jobs }) =>{
+  // simplificamos la logica fuera del jsx
+  if(jobs.length === 0) return  <article className="job-listing-card">
+    No se han encontrado empleos que coincidan con los criterios de búsqueda.
+  </article>
+
   return (
     <div className="jobs-listings">
-      {jobs.length === 0 && (
-        <article className="job-listing-card">
-          No se han encontrado empleos que coincidan con los criterios de búsqueda.
-        </article>
-      )}
-
       {jobs.map((job) => (
         <JobCard key={job.id} job={job} />
       ))}
